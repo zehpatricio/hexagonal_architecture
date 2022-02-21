156fdd9e49e70094816ea14e6aae9bb660f6e8db
José Patrício <jose.filho@dmz.org.br>
2022-02-21 12:06:59 -0300

    order locations list by date

--- app/domain/use_cases/location/list_location_use_case.py
+++ app/domain/use_cases/location/list_location_use_case.py
@@ -7,4 +7,7 @@ from app.domain.use_cases.location import base_location_use_case
 class ListLocationUseCase(base_location_use_case.BaseLocationUseCase):
 
     def list(self, user_id=None, device_id=None) -> List[Location]:
-        return self.repository.list(user_id=user_id, device_id=device_id)
+        locations = self.repository.list(user_id=user_id, device_id=device_id)
+        locations.sort(key=lambda l: l.date)
+
+        return locations
